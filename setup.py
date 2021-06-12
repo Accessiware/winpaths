57eb1f0fb45e150391463263598b0ac18eb171eb
NicklasTegner <NicklasMCHD@live.dk>
2021-06-12 16:38:26 +0200

    code commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import winpaths
+
+setup (name='winpaths',
+       version=winpaths.__version__,
+       description=winpaths.__description__,
+       author=winpaths.__author__,
+       py_modules=["winpaths"])
